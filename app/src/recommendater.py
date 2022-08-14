c7d09e3188fe1f6364bf53fd1bb559ba598b2d16
techlent2022summer3 <techlent2022summer3@gmail.com>
2022-08-14 14:17:07 -0500

    Fix bug

--- app/src/recommendater.py
+++ app/src/recommendater.py
@@ -7,7 +7,7 @@ class Recommendater:
     def method1(self):
         data = {
             "user id": [420, 380, 390],
-            "movie title": ['the godfather', 'Forrest Gump', 'The Dark Knight']
+            "movie title": ['the godfather', 'Forrest Gump', 'The Dark Knight'],
             "genres": ['11', '22', '33']
         }
 
