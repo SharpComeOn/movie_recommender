f4d894e4c92dbf1123c502ae72ab378ac2be00fd
techlent2022summer3 <techlent2022summer3@gmail.com>
2022-08-17 16:28:53 -0500

    Add functionality

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from src.recommendater import Recommendater
 from flask import Flask, render_template, request
 app = Flask(__name__)
@@ -5,27 +6,39 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     message = ""
-    user_id = 0
+    user_id = '0'
     action = ""
+    movie_title = ""
     if request.method == "POST":
         user_id = request.form["user_id"]
-        movie_title = request.form["movie_title"]
+        movie_title = request.form["movie_title"].title().strip()
         action = request.form["action"]
+        if (user_id.isnumeric() and int(user_id) > 0):
+            message = "Welcome " + str(user_id)
 
-        message = "Welcome " + str(user_id) + ", you clicked button of " + action
+    pd.set_option('colheader_justify', 'center')
 
     if (action == 'Content based'):
-        return render_template("index.html", welcome_message=message, tables=[genres_base(movie_title).to_html(classes='data', header="true")], titles=genres_base(movie_title).columns.values)
+        if(movie_title != ""):
+            df = genres_based(movie_title)
+            if(isinstance(df, pd.DataFrame)):
+                return render_template("index.html", welcome_message=message, tables=[df.to_html(classes='table_style', header="true")], titles=df.columns.values)
+        else:
+            return render_template("index.html", welcome_message="Please input a movie title to get recommendations of the same genres.")
+    elif (action == 'Memory based' and user_id.isnumeric() and int(user_id) > 0):
+        df = memory_based(user_id)
+        if isinstance(df, pd.DataFrame):
+            return render_template("index.html", welcome_message=message, tables=[df.to_html(classes='table_style', header="true")], titles=df.columns.values)
     else:
-        return render_template("index.html", welcome_message=message, tables=[method2(user_id).to_html(classes='data', header="true")], titles=method2(user_id).columns.values)
+        return render_template("index.html")
 
-def genres_base(movie_title):
-    rec = Recommendater(movie_title)
-    return rec.genres_base(movie_title)
+def genres_based(movie_title):
+    rec = Recommendater()
+    return rec.genres_based(movie_title)
 
-def method2(user_id):
-    rec = Recommendater(user_id, "Content based")
-    return rec.method2(user_id)
+def memory_based(user_id):
+    rec = Recommendater(user_id, "memory based")
+    return rec.memory_based(user_id)
 
 
 if __name__ == '__main__':
--- app/src/recommendater.py
+++ app/src/recommendater.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from ast import literal_eval
-from sklearn.metrics.pairwise import cosine_similarity
 from datetime import datetime
-# from surprise import Reader, Dataset, SVD
-# from surprise.model_selection import KFold, cross_validate
+from surprise import Reader, Dataset, SVD
+from surprise.model_selection import KFold, cross_validate
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from itertools import combinations
+from scipy.sparse import csr_matrix
 # from surprise.accuracy import rmse
-# from collections import defaultdict
+from collections import defaultdict
 # from surprise import accuracy
 
 class Recommendater:
-    def __init__(self, user_id, method=''):
+    def __init__(self, user_id=0, method=''):
         self.user_id = user_id
         self.method = method
 
@@ -20,16 +20,15 @@ class Recommendater:
         self.links_small = pd.read_csv('./data/links_small.csv', low_memory=False)
         self.meta_data = pd.read_csv('./data/movies_metadata.csv', low_memory=False)
         self.ratings_small = pd.read_csv('./data/ratings_small.csv', low_memory=False)
-
-
-    def genres_base(self, movie_title):
-
         self.meta_data['genres'] = self.meta_data['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else []).apply(lambda x: ','.join([str(i) for i in x]))
-        movies = self.meta_data[['id', 'title', 'genres']]
+        self.movies = self.meta_data[['id', 'title', 'genres', 'vote_average', 'homepage']]
+
+    def genres_based(self, movie_title):
+
         tf = TfidfVectorizer(analyzer=lambda s: (c for i in range(1,4) for c in combinations(s.split(','), r=i)))
-        tfidf_matrix = tf.fit_transform(movies['genres'])
+        tfidf_matrix = tf.fit_transform(self.movies['genres'])
         cosine_sim = cosine_similarity(tfidf_matrix)
-        movies = movies.reset_index()
+        movies = self.movies.reset_index()
         titles = movies['title']
         indices = pd.Series(movies.index, index=movies['title'])
         idx = indices[movie_title]
@@ -38,18 +37,55 @@ class Recommendater:
         sim_scores = sim_scores[1:31]
         movie_indices = [i[0] for i in sim_scores]
         recommendations = titles.iloc[movie_indices].head(10)
-        data = movies.merge(recommendations.to_frame(), left_index=True, right_index=True)[['title_x', 'genres']]
-        print(data)
-        return data
+        data = movies.merge(recommendations.to_frame(), left_index=True, right_index=True)
+        data.rename(columns={'title_x': 'Movie Title', 'genres': 'Genres', 'vote_average': 'Vote Average', 'homepage': 'Home Page'}, inplace=True)
+        data.reset_index(drop=True, inplace=True)
+        return data[['Movie Title', 'Genres', 'Vote Average', 'Home Page']]
 
+    def get_top_n(self, predictions, n=10):
+        """Return the top-N recommendation for each user from a set of predictions.
+        Args:
+            predictions(list of Prediction objects): The list of predictions, as
+                returned by the test method of an algorithm.
+            n(int): The number of recommendation to output for each user. Default
+                is 10.
+        Returns:
+        A dict where keys are user (raw) ids and values are lists of tuples:
+            [(raw item id, rating estimation), ...] of size n.
+        """
 
-    def method1(self, user_id):
-        data = {
-            "user id": [420, 380, 390],
-            "movie title": ['The godfather', 'Forrest Gump', 'The Dark Knight'],
-            "genres": [str(user_id) + '11', str(user_id) + '22', str(user_id) + '33']
-        }
-        return(pd.DataFrame(data))
+        # First map the predictions to each user.
+        top_n = defaultdict(list)
+        for uid, iid, true_r, est, _ in predictions:
+            top_n[uid].append((iid, est))
+
+        # Then sort the predictions for each user and retrieve the k highest ones.
+        for uid, user_ratings in top_n.items():
+            user_ratings.sort(key=lambda x: x[1], reverse=True)
+            top_n[uid] = user_ratings[:n]
+
+        return top_n
+
+    def memory_based(self, user_id='1'):
+        reader = Reader()
+        data = Dataset.load_from_df(self.ratings_small[['userId', 'movieId', 'rating']], reader)
+        svd = SVD()
+        trainset = data.build_full_trainset()
+        svd.fit(trainset)
+        testset = trainset.build_anti_testset()
+        predictions = svd.test(testset)
+        top_n = self.get_top_n(predictions, 10)
+
+        result = ""
+        df_recommendation = pd.DataFrame(columns=['id'])
+        if (user_id.isnumeric() and int(user_id) > 0):
+            for item in top_n.get(int(user_id)):
+                df_recommendation = df_recommendation.append({'id': item[0]}, ignore_index=True)
+
+            result = df_recommendation.merge(self.movies, on='id', how='left')
+            result.rename(columns={'title': 'Movie Title', 'genres': 'Genres', 'vote_average': 'Vote Average', 'homepage': 'Home Page'}, inplace=True)
+
+        return result
 
     def method2(self, user_id):
         data = {
